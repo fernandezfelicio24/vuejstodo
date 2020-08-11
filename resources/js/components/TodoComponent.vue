3f7815aeb305398bdb480b94aa8bd549c8263049
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2020-08-12 00:15:18 +0700

    nambah file tambah dari view

---
+++ resources/js/components/TodoComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-25">
+    <form>
+      <div class="input-group mb-3 w-100">
+            <input type="text" class="form-control form-control-lg" placeholder="" aria-label="Recipient's username" aria-describedby="button-addon2">
+            <div class="input-group-append">
+            <button class="btn btn-success" type="submit" id="button-addon2">Add this to your list</button>
+            </div>
+        </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    }
+  },
+
+  mounted() {
+
+  }
+}
+</script>
